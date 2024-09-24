43ef5e18fc59e1cb93c0b3794cac0ae9fb45ad3c
chan000518 <wkdvnddl050701@gmail.com>
2024-09-25 01:26:04 +0900

    feat: 에러 핸들러, 동기 핸들러 구현
    관련 주석 및 사용 예시 작성, test api 작성하여 테스트 완
    
    Resolves: #1 RelatedTo: #1 #2

---
+++ src/middlewares/errorHandler.js
@@ -0,0 +1,68 @@
+// 에러 코드 및 기본 메세지
+// 필요에 따라 추가 및 수정
+const ErrorCodes = {
+    BadRequest: {
+        code: 400,
+        message: "잘못된 요청입니다"
+    },
+    NotFound: {
+        code: 404,
+        message: "리소스를 찾을 수 없습니다"
+    },
+    InternalServerError: {
+        code: 500,
+        message: "서버 내부 오류가 발생했습니다"
+    },
+    Unauthorized: {
+        code: 401,
+        message: "인증되지 않은 요청입니다"
+    },
+    Forbidden: {
+        code: 403,
+        message: "접근 권한이 없습니다"
+    }
+};
+
+// 커스텀 에러 클래스
+// 생성자의 인자가 하나일 경우 해당 에러 코드의 메세지 삽입
+// 생성자의 인자가 두개일 경우 두번째 인자의 메세지 삽입
+class CustomError extends Error {
+    constructor(errorCode, message) {
+        super(message || errorCode.message);
+        this.code = errorCode.code;
+    }
+}
+
+// errorHandler.js
+// app.js 에러 핸들러로 모든 라우트 관리 
+// 필요에 따라 에러 처리 추가
+// 커스텀 에러가 아닌 에러에 대한 추가 필요!
+const errorHandler = (err, req, res, next) => {
+    console.error(err.message);
+
+    if (err instanceof CustomError) {
+        return res.status(err.code).json({
+            message: err.message,
+        });
+    }
+    
+    // 프리즈마 에러일 경우
+    // if (err instanceof StructError || err instanceof Prisma.PrismaClientValidationError) {
+    //     res.status(400).json({ message: '잘못된 요청입니다.' });
+    
+
+    // 예상치 못한 에러의 경우
+    res.status(500).json({
+        message: "예상치 못한 오류가 발생했습니다"
+    });
+};
+
+// 사용 예시
+
+// // 기본 메시지 사용
+// throw new CustomError(ErrorCodes.BadRequest);
+
+// // 커스텀 메시지 사용
+// throw new CustomError(ErrorCodes.NotFound, "사용자를 찾을 수 없습니다.");
+
+module.exports = { ErrorCodes, CustomError, errorHandler };
--- src/routers/index.js
+++ src/routers/index.js
@@ -1,38 +1,14 @@
 const express = require('express');
 const router = express.Router();
+const testRouters = require("./testRouters")
 
-
-
-
-
-
-
-
-
+//라우터 패스 
 /**
  * @swagger
  * tags:
  *   name: Test
  *   description: 테스트 API 관련 엔드포인트
  */
-
-/**
- * @swagger
- * /hi:
- *   get:
- *     summary: "안녕하세요 메시지 반환"
- *     tags: [Test]
- *     responses:
- *       200:
- *         description: "성공적으로 메시지를 반환합니다."
- *         content:
- *           text/plain:
- *             schema:
- *               type: string
- *               example: "Hello"
- */
-router.get("/hi", (req, res) => {
-    res.send('Hello');
-});
+router.use("", testRouters);
 
 module.exports = router;
