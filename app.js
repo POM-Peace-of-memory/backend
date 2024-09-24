ef29fe04edb8a9c732919e4f798a61458cbb4ce7
chan000518 <wkdvnddl050701@gmail.com>
2024-09-25 01:26:04 +0900

    feat: 에러 핸들러, 동기 핸들러 구현 (관련 주석 및 사용 예시 작성, test api 작성하여 테스트 완) resolves: #1 related by #1 #2

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const api = require("./src/routers/index")
 const app = express();
+const {errorHandler} = require("./src/middlewares/errorHandler")
 
 app.use("/api", api);
 
@@ -8,5 +9,6 @@ const { swaggerUi, specs } = require("./docs/swagger");
 
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(specs));
 
+app.use(errorHandler);
 // 서버 시작
 app.listen(3000, () => console.log('Server Started'));
