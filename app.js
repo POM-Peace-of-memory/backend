fac4c3d1279ae6671cb5cc9317ccddd3b0fd2a41
chan000518 <wkdvnddl050701@gmail.com>
2024-09-25 00:03:52 +0900

    docs: Swagger 설치 및 초기 설정
    test api로 스웨거 테스트
    
    RelatedTo #1

--- app.js
+++ app.js
@@ -1,5 +1,12 @@
 const express = require('express');
+const api = require("./src/routers/index")
 const app = express();
+
+app.use("/api", api);
+
+const { swaggerUi, specs } = require("./docs/swagger");
+
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(specs));
 
 // 서버 시작
 app.listen(3000, () => console.log('Server Started'));
---
+++ docs/swagger.js
@@ -0,0 +1,24 @@
+const swaggerUi = require("swagger-ui-express");
+const swaggerJsdoc = require("swagger-jsdoc");
+
+const options = {
+  definition: {
+    openapi: "3.0.0",
+    info: {
+      version: "1.0.0",
+      title: "Peace of Memory",
+      description: "(POM) Peace of Memory : Node.js, express restful api project (CodeItPB-ToyProject)",
+    },
+    servers: [
+      {
+        url: "http://localhost:3000/", 
+        description: "API server",
+      },
+    ],
+  },
+  apis: ["./src/routers/*.js"], 
+};
+
+const specs = swaggerJsdoc(options);
+
+module.exports = { swaggerUi, specs };
