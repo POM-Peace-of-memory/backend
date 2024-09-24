ffb5c46585b4d049c91bf37a4de16fcf9bb8c734
chan000518 <wkdvnddl050701@gmail.com>
2024-09-25 01:26:04 +0900

    feat: 에러 핸들러, 동기 비동기 핸들러 구현
    에러를 미들웨어에서 관리
    - 에러코드 및 기본 메세지를 관리
    - 메세지는 커스텀이 가능하도록 생성자 작성
    관련 주석 및 사용 예시 작성
    test api 작성하여 테스트 완
    
    resolves: #1 RelatedTo: #1 #2

---
+++ src/routers/testRouters.js
@@ -0,0 +1,46 @@
+const express = require('express');
+const asyncHandler = require("../middlewares/asyncHandler")
+const {ErrorCodes, CustomError} = require("../middlewares/errorHandler")
+const router = express.Router();
+
+// 실제 등록시 
+// router.[method]("[path]", asyncHandler([controller.function]))
+// api에 맞게 메소드, 경로 , 핸들러에 감싸서 컨트롤러의 함수 받으면 됩니다
+// 에러 throw시 커스텀 에러 생성해서 던져 주세요!!
+
+// 테스트 api : hi
+router.get("/hi", asyncHandler((req, res) => {
+    res.send('Hello');
+}));
+/**
+ * @swagger
+ * /api/hi:
+ *   get:
+ *     summary: "안녕하세요 메시지 반환"
+ *     tags: [Test]
+ *     responses:
+ *       200:
+ *         description: "성공적으로 메시지를 반환합니다."
+ *         content:
+ *           text/plain:
+ *             schema:
+ *               type: string
+ *               example: "Hello"
+ */
+
+// 테스트 api : error
+router.get("/testErr", asyncHandler((req, res) => {
+    throw new CustomError(ErrorCodes.BadRequest);
+}));
+/**
+ * @swagger
+ * /api/testErr:
+ *   get:
+ *     summary: "테스트 오류 발생"
+ *     tags: [Test]
+ *     responses:
+ *       400:
+ *         description: "잘못된 요청입니다."
+ */
+
+module.exports = router;
