6c49648f0a1be9fa4a5279482324173f01788485
chan000518 <wkdvnddl050701@gmail.com>
2024-09-28 16:48:59 +0900

    feat: 유틸 함수 작성(비밀번호,날짜 관련)
    날짜 규격화(년-월-일)
    - Date 와 string을 타입과 규격에 맞게 변환해주는 함수 작성
    비밀번호
    - 암호화(해시)함수, 저장된 암호화 비밀번호와 비교하는 함수 작성
    
    RelatedTo: #10 #11

---
+++ src/utill/passwordUtils.js
@@ -0,0 +1,17 @@
+import * as bcrypt from 'bcrypt';
+
+// 비밀번호 해싱 함수
+// 비동기 방식으로 처리하여 서버 성능을 최적화합니다.
+// - 해싱 작업은 CPU 집약적 작업, 비동기로 처리하지 않으면 
+//   서버가 해당 작업을 기다리며 블로킹이 생길 수 있음
+async function hashPassword(password) {
+    const salt = await bcrypt.genSalt(10);
+    return await bcrypt.hash(this.password, salt);
+}
+
+// 비밀번호 비교 함수
+async function comparePassword(plainPassword, hashedPassword) {
+  return bcrypt.compare(plainPassword, hashedPassword);
+}
+
+module.exports = {hashPassword, comparePassword}
