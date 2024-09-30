f14a31ab8f0a99b53d7152100a36ba1b922f02fc
chan000518 <wkdvnddl050701@gmail.com>
2024-09-30 17:35:55 +0900

    hotFix: cors 설정 추가
    cors 설정 추가
    - 프론트 로컬 테스트 가능하게
    - localhost 허용
    
    RelatedTo: #18

--- app.js
+++ app.js
@@ -3,6 +3,16 @@ const api = require("./src/routers/index")
 const app = express();
 const {errorHandler} = require("./src/middlewares/errorHandler")
 
+// CORS 옵션 설정: 로컬 호스트에 대해서만 접근 허용
+const corsOptions = {
+    origin: 'http://localhost:3000', // 허용할 로컬 도메인
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE', // 허용할 HTTP 메서드
+    credentials: true, // 쿠키와 같은 인증 정보 허용
+};
+  
+  // CORS 미들웨어 사용
+app.use(cors(corsOptions));
+  
 app.use("/api", api);
 
 const { swaggerUi, specs } = require("./docs/swagger");
